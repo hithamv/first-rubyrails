896b4311513587d92994cb3c047da9b99167757b
Hitha M V <hithamv@Hithas-MacBook-Air.local>
2024-09-28 18:01:06 -0500

    highlighting the column sorted add helpers functions for sort and highlight

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,24 @@
 module ApplicationHelper
-    def sortable(column, title = nil)
-        title ||= column.titleize
-        direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"
-        css_class = column == sort_column ? "sortable active #{sort_direction}" : "sortable"
-        link_to title, { sort: column, direction: direction }, { class: css_class }
-      end
+  def sortable(column, title = nil)
+    title ||= column.titleize
+    direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"
+    icon = (column == sort_column) ? (sort_direction == "asc" ? " ▲" : " ▼") : ""
+    link_to "#{title}#{icon}".html_safe, { sort: column, direction: direction }
+  end
+
+  def sort_column
+    Movie.column_names.include?(params[:sort]) ? params[:sort] : "title"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+  end
+
+  def highlight_column(column)
+    if column == sort_column
+      "background-color: #ff9800; color: white; border-bottom: 2px solid #dddddd;"
+    else
+      "background-color: #007bff; color: white; border-bottom: 2px solid #dddddd;"
+    end
+  end
 end
