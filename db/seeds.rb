59b5a01c57cc0208bc212f0a95e540db01e54ab1
Hitha M V <hithamv@Hithas-MacBook-Air.local>
2024-09-29 21:00:50 -0500

    seeding 3 more movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,6 +18,12 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
+    :release_date => '13-Aug-2021'},
+  {:title => 'SVT', :rating => 'R',
+    :release_date => '12-May-2017'},
+  {:title => 'BTS', :rating => 'PG-13',
+    :release_date => '12-Feb-2011'},
+  {:title => 'Kantara', :rating => 'PG-13',
     :release_date => '13-Aug-2021'}
 ]
 
